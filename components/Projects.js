d88da8c3eb9b0587e4705b349aaa5fb73c2b0578
Ogada Stanley Chinedu <54116871+chineduogada@users.noreply.github.com>
2021-07-31 15:52:56 +0100

    Update Projects.js

--- components/Projects.js
+++ components/Projects.js
@@ -6,28 +6,32 @@ import ButtonLink from "./ButtonLink";
 
 const projects = [
   {
-    github: "https://github.com/chineduogada/e-cormerce",
-    vercel: "https://e-cormerce.vercel.app/",
-    heading: "E-cormerce template",
+    github: "https://github.com/chineduogada",
+    vercel: "https://www.silverdream.ng",
+    heading: "SilverDream.ng",
     description:
-      "An e-commerce Next.js template starter, which has Dark-Light mode",
+      "An E-commerce platform owned by Silver Dream, for selling products to their new and existing customers...",
   },
   {
-    github: "https://github.com/chineduogada/my-portfolio",
-    heading: "My Portfolio",
-    description: "My Curriculum vitae",
+    github: "https://github.com/chineduogada/deal-vend",
+    heading: "DealVend",
+    description: "An E-commerce side project, with authentication, homepage, product-detail-page...",
+  },
+
+  {
+
+    github: "https://github.com/chineduogada/deal-vend",
+vercel: 'https://deal-vend.herokuapp.com/api/v1/products',
+    heading: "DealVend API",
+
+    description: "The REST api for DealVend client, built using NodeJs, Express, JWT (authentication), MongoDB, etc.",
+
   },
   {
     github: "https://github.com/chineduogada/react-rapid-carousel",
     npm: "https://www.npmjs.com/package/react-rapid-carousel",
     heading: "React Rapid Carousel",
     description: "A simple but epic React Responsive Carousel.",
-  },
-  {
-    github: "https://github.com/chineduogada/sellit",
-    heading: "SellIT.ng",
-    description:
-      "A website that connects buyers and sellers together, both locally and internationally",
   },
 ];
 
